0ed06cdbf8ca7172c6767d3a4b98f72949b31f90
evgenuy77 <evgenuy77@gmail.com>
2021-04-18 01:47:01 +0300

    Добавьте файлы проекта.

---
+++ OnlineShopWindowsApp/UserControls/SignInControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OnlineShopWindowsApp.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для SignInControl.xaml
+    /// </summary>
+    public partial class SignInControl : UserControl
+    {
+        public Action regClick{ get; set; }
+        public SignInControl()
+        {
+            InitializeComponent();
+        }
+
+        private void RegClick(object sender, RoutedEventArgs e)
+        {
+            regClick?.Invoke();
+        }
+    }
+}
