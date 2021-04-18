c5df6adf90a7e13f63d277930fbe54e84e3f4051
evgenuy77 <evgenuy77@gmail.com>
2021-04-18 22:00:43 +0300

    added CartPage

---
+++ OnlineShopWindowsApp/Pages/CartPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OnlineShopWindowsApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для CartPage.xaml
+    /// </summary>
+    public partial class CartPage : Page, INotifyPropertyChanged
+    {
+        //Для scrollviewer во 2-й строке
+        public double Row1Height { get; set; }
+        public CartPage()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void PageSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Row1Height = this.ActualHeight-150;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Row1Height"));
+        }
+    }
+}
---
+++ OnlineShopWindowsApp/Pages/TestItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShopWindowsApp.Pages
+{
+    public class TestItem
+    {
+        public string ItemName { get; set; }
+        public string ItemPrice { get; set; }
+        public string Count { get; set; }
+    }
+}
